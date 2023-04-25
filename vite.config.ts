39c947494c17178babcd6297697bc5eda7dddb2a
Danniel Navas <dannielnavas@gmail.com>
2023-04-25 08:29:41 -0500

    🐛 route assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://dannielnavas.github.io/",
+  base: "https://dannielnavas.github.io/github-actions/",
   plugins: [react()],
 });
